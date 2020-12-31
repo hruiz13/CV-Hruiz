e2b86d01c0f19c8b66430f33c94035f5712d9161
Hector Ruiz <hruiz.com@gmail.com>
2020-12-31 08:30:58 -0500

    solving router problem

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { AppRouter } from './routers/AppRouters';
-import 'typeface-roboto';
 
 
 ReactDOM.render(
